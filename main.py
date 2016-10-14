dde3ca854cb8bfdb847e6d4fa3a793a0a54e8a59
Lastdier <a499340016@gmail.com>
2016-10-14 09:52:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from queue import Queue
 from spider import Spider
 
 PROJECT_NAME = 'JD'
-NUMBER_OF_THREAD = 16
+NUMBER_OF_THREAD = 16                          # 线程数
 queue = Queue()
 
 Spider(PROJECT_NAME)
@@ -11,7 +11,7 @@ Spider(PROJECT_NAME)
 
 def create_worker():
     for _ in range(NUMBER_OF_THREAD):
-        w = threading.Thread(target=work)
+        w = threading.Thread(target=work)       # work函数指定每个线程的工作
         w.daemon = True
         w.start()
 
@@ -23,13 +23,11 @@ def work():
             Spider.crawl_class(threading.current_thread().name, url)
         except:
             print(threading.current_thread().name + 'work failed!')
-        queue.task_done()
+        finally:
+            queue.task_done()
 
 
 create_worker()
 for link in Spider.list_link:
     queue.put(link)
 queue.join()
-
-
-
